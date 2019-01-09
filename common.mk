AICP/device_oneplus_sdm845-common

sdm845-common: Bind mount our Custom  Equilizer (JamesDSP)
Based on https://github.com/LineageOS/android_device_oneplus_sdm845-common/commit/1272127b9a22c8d6bae39e351006cfa30edd0d10

Change-Id: Ia2337ce4810cc9b4b1ba54247ff31abb5f471ea6
Signed-off-by: doc HD <doc.divxm@gmail.com>
Signed-off-by: LuK1337 <priv.luk@gmail.com>

common.mk
@@ -51,6 +51,7 @@ PRODUCT_PACKAGES += \
     audio.a2dp.default
 
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/audio/audio_effects.conf:system/etc/audio_effects.conf \
     $(LOCAL_PATH)/audio/audio_policy_configuration.xml:system/etc/audio_policy_configuration.xml
 
 # Boot control

hardwareadictos/val_device_oneplus_sdm845-common

common.mk
@@ -51,6 +51,7 @@ PRODUCT_PACKAGES += \
     audio.a2dp.default
 
 PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/audio/audio_effects.xml:system/etc/audio_effects.xml \
     $(LOCAL_PATH)/audio/audio_policy_configuration.xml:system/etc/audio_policy_configuration.xml
 
 # Boot control
@@ -155,7 +156,7 @@ PRODUCT_PACKAGES_DEBUG += \
 
 #Wifi
 PRODUCT_COPY_FILES += \
-    $(LOCAL_PATH)/prebuilts/etc/WCNSS_qcom_cfg.ini:/vendor/etc/wifi/WCNSS_qcom_cfg.ini 
+    $(LOCAL_PATH)/prebuilts/etc/WCNSS_qcom_cfg.ini:/vendor/etc/wifi/WCNSS_qcom_cfg.ini
 
 # WFD
 PRODUCT_BOOT_JARS += \